994f73965a7ec9b69c3eb29ee31f5116fb4ec0b0
nadinedelia <nadine.delia@yahoo.com>
2020-09-07 20:49:31 +0200

    added eventlistener and timeline

--- app2.js
+++ app2.js
@@ -1,1 +1,20 @@
 const moonPath = "M54 100C54 155.228 100 200 100 200C44.7715 200 0 155.228 0 100C0 44.7715 44.7715 0 100 0C100 0 54 44.7715 54 100Z"
+
+const sunPath = "M200 100C200 155.228 155.228 200 100 200C44.7715 200 0 155.228 0 100C0 44.7715 44.7715 0 100 0C155.228 0 200 44.7715 200 100Z"
+
+const darkMode = document.querySelector('#darkmode');
+let toggle = false;
+
+darkMode.addEventListener('click', () => {
+    const timeline = anime.timeline({
+        duration : 750, 
+        easing : "easeOutExpo"
+    });
+
+    timeline.add({
+        targets: ".sun",
+        d: [
+            {value: moonPath}
+        ]
+    })
+});
